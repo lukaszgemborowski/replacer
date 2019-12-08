b5a7c87cd3772f06ca6ccaf2d0762f504bd62b30
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2019-12-08 23:59:50 +0100

    move logic to application class

--- src/main.cxx
+++ src/main.cxx
@@ -44,75 +44,91 @@ struct options
     }
 };
 
-template<class NcList>
-void match_in_file(
-    NcList &list,
-    const fs::path &p,
-    const std::regex &re,
-    const std::string &with)
+
+struct application
 {
-    std::string line;
-    std::ifstream fs{p};
+    application(ncursespp::session &ses, const fs::path &path, const std::regex &re, const std::string &with)
+        : ses_ {ses}
+        , path_ {path}
+        , re_ {re}
+        , with_ {with}
+        , list_ {}
+    {
+    }
 
-    list.append(replacer::view_item{p});
+    int run()
+    {
+        for (auto &p : fs::recursive_directory_iterator{path_}) {
+            if (fs::is_regular_file(p.status())) {
+                if (p.path().filename().string()[0] == '.')
+                    continue;
+                match_in_file(p.path());
+            }
+        }
 
-    while (std::getline(fs, line)) {
-        auto m = std::sregex_iterator(line.begin(), line.end(), re);
-        auto e = std::sregex_iterator();
+        list_.resize(ses_.size());
+        list_.select(0);
+        list_.resize(ses_.size());
+        ses_.refresh();
 
-        for (auto i = m; i != e; ++i) {
-            auto match = *i;
+        int c = 0;
 
-            list.append(
-                replacer::view_item{
-                    p,
-                    line,
-                    match.position(),
-                    match.length(),
-                    re,
-                    with
-                }
-            );
+        while (c != 'q') {
+            c = ses_.getkey();
+
+            if (c == 'k') {
+                list_.select_up();
+            } else if (c == 'j') {
+                list_.select_down();
+            }
+        }
+
+        return 0;
+    }
+
+    void match_in_file(const fs::path &p)
+    {
+        std::string line;
+        std::ifstream fs{p};
+
+        list_.append(replacer::view_item{p});
+
+        while (std::getline(fs, line)) {
+            auto m = std::sregex_iterator(line.begin(), line.end(), re_);
+            auto e = std::sregex_iterator();
+
+            for (auto i = m; i != e; ++i) {
+                auto match = *i;
+
+                list_.append(
+                    replacer::view_item{
+                        p,
+                        line,
+                        match.position(),
+                        match.length(),
+                        re_,
+                        with_
+                    }
+                );
+            }
         }
     }
-}
+
+    ncursespp::session &ses_;
+    const fs::path path_;
+    const std::regex &re_;
+    const std::string &with_;
+    ncursespp::item_list<replacer::view_item> list_;
+};
 
 
 int main(int argc, char **argv)
 {
     options opt{argc, argv};
-
     std::regex re{*opt.expression.value(), std::regex::optimize};
-
     auto ses = ncursespp::session{};
     replacer::create_theme();
 
-    auto list = ncursespp::item_list<replacer::view_item>{};
-
-    for (auto &p : fs::recursive_directory_iterator{opt.search_dir()}) {
-        if (fs::is_regular_file(p.status())) {
-            if (p.path().filename().string()[0] == '.')
-                continue;
-            match_in_file(list, p.path(), re, *opt.with.value());
-        }
-    }
-
-    list.resize(ses.size());
-    list.select(0);
-    list.resize(ses.size());
-    ses.refresh();
-
-    int c = 0;
-
-    while (c != 'q') {
-        c = ses.getkey();
-
-        if (c == 'k') {
-            list.select_up();
-        } else if (c == 'j') {
-            list.select_down();
-        }
-    }
-
-    return 0;
+    application app{ses, opt.search_dir(), re, *opt.with.value()};
+    return app.run();
 }
