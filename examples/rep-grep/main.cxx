4d363b16ecf8f6f2a6c11196cad6e7b7cb9983dc
Łukasz Gemborowski <lukasz.gemborowski@gmail.com>
2020-02-19 20:34:46 +0100

    example: improve search and replace

--- examples/rep-grep/main.cxx
+++ examples/rep-grep/main.cxx
@@ -1,23 +1,58 @@
 #include <replacer/IterateFilesystem.hpp>
 #include <iostream>
 
-void print_entry(const fs::path &path, const replacer::LineMatches &lm, replacer::MatchSpan span)
+namespace
 {
-    std::cout << path << ":" << lm.lineNumber() << ": "
-        << lm.beforeMatch(span)
-        << "\x1b[32m" << lm.matchedString(span) << "\x1b[0m"
-        << lm.afterMatch(span) << std::endl;
+const auto TEXT_GREEN = "\x1b[32m";
+const auto TEXT_RED = "\x1b[31m";
+const auto TEXT_RESET = "\x1b[0m";
 }
+
+class GrepPrinter
+{
+public:
+    void operator()(const fs::path &path, const replacer::LineMatches &lm, replacer::MatchSpan span)
+    {
+        std::cout << path << ":" << lm.lineNumber() << ": "
+            << lm.beforeMatch(span)
+            << TEXT_GREEN << lm.matchedString(span) << TEXT_RESET
+            << lm.afterMatch(span) << std::endl;
+    }
+};
+
+class ReplacePrinter
+{
+public:
+    ReplacePrinter(std::string_view replaceWith)
+        : replaceWith_ {replaceWith}
+    {}
+
+    void operator()(const fs::path &path, const replacer::LineMatches &lm, replacer::MatchSpan span)
+    {
+        std::cout << path << ":" << lm.lineNumber()
+                  << lm.beforeMatch(span)
+                  << TEXT_RED << lm.matchedString(span)
+                  << TEXT_GREEN << std::regex_replace(std::string{lm.matchedString(span)}, lm.regexp(), replaceWith_)
+                  << TEXT_RESET << lm.afterMatch(span) << std::endl;
+    }
+
+private:
+    std::string replaceWith_;
+};
 
 int main(int argc, char **argv)
 {
-    if (argc != 3) {
-        std::cout << argv[0] << ": [pattern] [path]" << std::endl;
+    if (argc == 4) {
+        std::regex re{argv[1]};
+        replacer::IterateFilesystemMatches(fs::path{argv[3]}, re, ReplacePrinter{argv[2]});
+    } else if (argc == 3) {
+        std::regex re{argv[1]};
+        replacer::IterateFilesystemMatches(fs::path{argv[2]}, re, GrepPrinter{});
+    } else {
+        std::cout << argv[0] << ": [search-pattern] [replace-pattern] [path]" << std::endl;
+        std::cout << argv[0] << ": [search-pattern] [path]" << std::endl;
         return 0;
     }
-
-    std::regex re{argv[1]};
-    IterateFilesystemMatches(fs::path{argv[2]}, re, print_entry);
 
     return 0;
 }
